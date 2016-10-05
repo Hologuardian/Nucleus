b64c4b5e36a777c7bb755626b0f3524dcaf3357a
aidandearing <aidan.dearing@live.com>
2016-10-05 18:17:46 -0400

    Testing Cloth
    Testing if cloth is a better way of simulating 'membrane' effects, it
    might work, I just have to figure out how to use it properly. It will be
    more expensive though.
    
    Ultimately I will work on constructing shaders.

---
+++ Nucleus/Assets/MoveTestScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 1.0f * Time.deltaTime, 0));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -1.0f * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-1.0f * Time.deltaTime, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(1.0f * Time.deltaTime, 0, 0));
+        }
+    }
+}

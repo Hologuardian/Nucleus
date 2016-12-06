d075196d8b000df9764d8e8d4e5e4e5800a8d72f
Liam Johnston <liamjohnston266@hotmail.ca>
2016-12-06 17:42:56 -0500

    Basic UI creation for a main menu, as well as a widget. Widget has a button for the rewards that can activate and deactivate a slider. The slider changes the value of the rewards which will be displayed on the button.

---
+++ Nucleus/Assets/Scripts/UI/ActionRewardWidget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActionRewardWidget : MonoBehaviour
+{
+    public Action action;
+    public Slider slider;
+    public GameObject sliderGO;
+    public Text text;
+    
+	
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+    public void SetTextValue()
+    {
+        text.text = slider.value.ToString();
+    }
+    public void ToggleSlider()
+    {
+        sliderGO.SetActive(!sliderGO.activeInHierarchy);
+    }
+}
---
+++ Nucleus/Assets/Scripts/UI/PlayGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    public void playGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

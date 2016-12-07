33012f7ad87f4d1e1331e210b33e865b7b47e1e3
aidandearing <aidan.dearing@live.com>
2016-12-07 11:48:26 -0500

    UI Widget Wars
    This is part one of many parts to get UI doing the do.

--- Nucleus/Assets/Scripts/UI/ActionRewardWidget.cs
+++ Nucleus/Assets/Scripts/UI/ActionRewardWidget.cs
@@ -6,9 +6,16 @@ public class ActionRewardWidget : MonoBehaviour
 {
     public Colony colony;
     public Text text;
+    public Image fill_top;
     public Image fill_bottom;
-    public Sprite icon;
+    public Image icon;
     public Slider slider;
+
+    private bool isVisible = false;
+    private float visibility = 1;
+
+    public float timer_goal = 0.2f;
+    private float timer = 0;
 	
 	void Start()
     {
@@ -17,7 +24,34 @@ public class ActionRewardWidget : MonoBehaviour
 	
 	void Update()
     {
-	
+        float timer_evaluated = timer / timer_goal;
+
+        if (isVisible)
+        {
+            if (visibility < 1)
+            {
+                visibility = Mathf.Lerp(0, 1, timer_evaluated);
+                timer += Time.deltaTime;
+
+                Color colour = fill_top.color;
+                Color colour_bottom = fill_bottom.color;
+                Color colour_text = text.color;
+                colour.a = colour_bottom.a = colour_text.a = Mathf.Lerp(0, 1, timer_evaluated);
+            }
+        }
+        else
+        {
+            if (visibility > 0)
+            {
+                visibility = Mathf.Lerp(1, 0, timer_evaluated);
+                timer += Time.deltaTime;
+
+                Color colour = fill_top.color;
+                Color colour_bottom = fill_bottom.color;
+                Color colour_text = text.color;
+                colour.a = colour_bottom.a = colour_text.a = Mathf.Lerp(1, 0, timer_evaluated);
+            }
+        }
 	}
 
     public void Initialise(string action, Colony colony)
@@ -25,7 +59,7 @@ public class ActionRewardWidget : MonoBehaviour
         text.text = action;
         this.colony = colony;
 
-        icon = Resources.Load<Sprite>("Sprites/" + action);
+        icon = Resources.Load<Image>("Sprites/" + action);
 
         ColorBlock colorBlock = slider.colors;
         // GlobalsSetter has a dictionary of action name to colour
@@ -35,5 +69,11 @@ public class ActionRewardWidget : MonoBehaviour
     public void ValueChange()
     {
         colony.actionRewards[text.text] = slider.value;
+    }
+
+    public void ToggleVisibility()
+    {
+        isVisible = !isVisible;
+        timer = 0;
     }
 }

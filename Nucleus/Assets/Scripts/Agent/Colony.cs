d2151a32ec2546327b43d86c48a3205936d4773e
aidandearing <aidan.dearing@live.com>
2016-12-07 15:43:36 -0500

    Networking and Synycs

--- Nucleus/Assets/Scripts/Agent/Colony.cs
+++ Nucleus/Assets/Scripts/Agent/Colony.cs
@@ -10,6 +10,18 @@ public class Colony : MonoBehaviour
     public List<string> availableActions = new List<string>();
     public Dictionary<string, float> actionRewards = new Dictionary<string, float>();
 
+    void Start()
+    {
+        Vector3 position = UnityEngine.Random.insideUnitCircle * 50.0f;
+
+        for(int i = 0; i < 10; i++)
+        {
+            Vector3 local = UnityEngine.Random.insideUnitCircle * 5.0f;
+
+            SimpleAgent cell = Network.Instantiate(cellPrefab, position + local, new Quaternion(), 0) as SimpleAgent;
+        }
+    }
+
     public void AddAction(Action action)
     {
         AddAction(action, 0.5f);
